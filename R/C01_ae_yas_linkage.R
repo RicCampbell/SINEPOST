6a518a4ea5c33e9abcddbc17c4ef5b134799f341
Richard Campbell <su_cm1rjcx@sheffield.ac.uk>
2021-07-27 16:04:01 +0100

    Parts that looked at +-24 hour links has been removed and moved to an ad-hoc script, but actual linkage on 0-6 hour time period remains. Changes to avoid wrong linkage as well (reduction of records before ordering) 24 hour re-attendance added

--- R/C01_ae_yas_linkage.R
+++ R/C01_ae_yas_linkage.R
@@ -16,7 +16,7 @@ source("R/cleaning_fns_etl.r")
   study_id_encrypted_hesid_lookup <- readRDS("data/linkage/study_id_hesid_lookup_2021-07-12-105942.rds")
   
 
-## Check only one  ENCRYPTED_HESID per AEKEY
+## Check only one ENCRYPTED_HESID per AEKEY
   
   stopifnot(ae_data[, .N, by = .(ENCRYPTED_HESID, AEKEY)][, .N, by = AEKEY][N > 1, .N] == 0)
   
@@ -33,7 +33,7 @@ source("R/cleaning_fns_etl.r")
 
 ## Merge in all AE arrival times for each person (HESID)
 
-  yas_hes_linkage <- merge(epr_single_value_fields_table[transported_to_hospital == TRUE & receiving_hospital_department == "Emergency Department",
+  yas_hes_linkage <- merge(epr_single_value_fields_table[!is.na(ENCRYPTED_HESID) & transported_to_hospital == TRUE & receiving_hospital_department == "Emergency Department",
                                                                     .(ENCRYPTED_HESID, incident_datetime, epr_id, patient_id)],
                            ae_data[, .(ENCRYPTED_HESID, ARRIVALTIME, AEKEY)],
                            by = "ENCRYPTED_HESID",
@@ -44,30 +44,29 @@ source("R/cleaning_fns_etl.r")
 
   yas_hes_linkage[, incident_to_arrival_time := as.numeric(difftime(ARRIVALTIME, incident_datetime, unit = "hours"))]
   
-  yas_hes_linkage[, incident_to_arrival_time_clipped := pmax(pmin(incident_to_arrival_time, 24.0), -24.0)]
   
-  yas_hes_linkage[, incident_to_arrival_time_absolute := abs(incident_to_arrival_time)]
-
-
-## Set order to find smallest absolute time between ambulance incident time and A&E arrival time
-
-  setorder(yas_hes_linkage, epr_id, incident_to_arrival_time_absolute, AEKEY, na.last = TRUE)
+## Reduce to only AE records that happened with first 6 hours of ambulance incident time - done here so as ordering does not put minus times first
   
-  yas_hes_linkage[!is.na(incident_to_arrival_time_absolute), order := 1:.N, by = epr_id]
+  yas_hes_linkage <- yas_hes_linkage[incident_to_arrival_time > 0 & incident_to_arrival_time < 6.0]
+
+  
+## Set order to find smallest time between ambulance incident time and A&E arrival time
+
+  setorder(yas_hes_linkage, epr_id, incident_to_arrival_time, AEKEY, na.last = TRUE)
+  
+  yas_hes_linkage[!is.na(incident_to_arrival_time), order := 1:.N, by = epr_id]
 
 
-### Select the smallest absolute time that was within 24 hours, but also remove any cases that were within the first and last day of data
+### Select the smallest absolute time that was after incident time and within 6 hours - check ad-hoc script for distribution of +-24 hours which led to this decision
 
-  smallest_time_diff_per_amb_inc_24_hour <- yas_hes_linkage[order == 1 &
-                                                              incident_datetime >= floor_date(min(incident_datetime), unit = "day") + days(1) &
-                                                              incident_datetime < floor_date(max(incident_datetime), unit = "day")][, order := NULL]
+  smallest_time_diff_per_amb_inc_24_hour <- yas_hes_linkage[order == 1][, order := NULL]
   
   
 ## Need to order the other way so as to keep AEKEY-epr_id 1-to-1. epr_id included in sort to keep same each time
 
-  setorder(smallest_time_diff_per_amb_inc_24_hour, incident_to_arrival_time_absolute, epr_id, na.last = TRUE)
+  setorder(smallest_time_diff_per_amb_inc_24_hour, incident_to_arrival_time, epr_id, na.last = TRUE)
   
-  smallest_time_diff_per_amb_inc_24_hour[!is.na(incident_to_arrival_time_absolute), order := 1:.N, by = AEKEY]
+  smallest_time_diff_per_amb_inc_24_hour[!is.na(incident_to_arrival_time), order := 1:.N, by = AEKEY]
   
   smallest_time_diff_per_amb_inc_24_hour <- smallest_time_diff_per_amb_inc_24_hour[order == 1]
 
@@ -76,9 +75,6 @@ source("R/cleaning_fns_etl.r")
   
   stopifnot(smallest_time_diff_per_amb_inc_24_hour[, .N, by = .(epr_id, AEKEY)][, .N, by = epr_id][N > 1, .N] == 0)
 
-  
-## Take records that are between 0 and 6 hours time duration, and only earliest in that time period
-## Label last 5 (less than 6) hours of YAS data for this - field that indicates records are in this time-frame
 
 ## Add prefixes to field names in each table, and drop/rename common fields (HES_ID) after
   
@@ -91,7 +87,7 @@ source("R/cleaning_fns_etl.r")
   
 
   yas_ae_data <- merge(merge(epr_single_value_fields_table,
-                             smallest_time_diff_per_amb_inc_24_hour[incident_to_arrival_time > 0 & incident_to_arrival_time < 6.0, .(epr_id, AEKEY)],
+                             smallest_time_diff_per_amb_inc_24_hour[, .(epr_id, AEKEY)],
                              by = "epr_id",
                              all.x = TRUE),
                        ae_data,
@@ -105,6 +101,11 @@ source("R/cleaning_fns_etl.r")
   yas_ae_data[, epr_ENCRYPTED_HESID := NULL]
 
   
+## Re-make time to ae field
+  
+  yas_ae_data[, ambulance_incident_to_ae_arrival := as.numeric(difftime(ae_ARRIVALTIME, epr_incident_datetime, unit = "hours"))]
+
+    
 ## Check that each epr_id is still only present once as only taken the first AE record per epr
   
   stopifnot(yas_ae_data[, .N, by = epr_id][N > 1, .N] == 0)
@@ -115,35 +116,59 @@ source("R/cleaning_fns_etl.r")
   
 ## Check that all AEKEYS found within 0-6 hours are present
   
-  stopifnot(setdiff(smallest_time_diff_per_amb_inc_24_hour[incident_to_arrival_time > 0 & incident_to_arrival_time < 6.0, AEKEY], yas_ae_data[, AEKEY]) == 0)
+  stopifnot(setdiff(smallest_time_diff_per_amb_inc_24_hour[, AEKEY], yas_ae_data[, AEKEY]) == 0)
+  
+## Check all times are between 0 and 6 hours, or NA
+  
+  stopifnot(yas_ae_data[ambulance_incident_to_ae_arrival < 0 & ambulance_incident_to_ae_arrival > 6.0, .N] == 0)
 
 ## Check that all epr_ids were kept
   
   stopifnot(yas_ae_data[, .N] == epr_single_value_fields_table[, .N])
 
-  
-## **** Extra
+
+# Non-conveyed AE attendances ----------------------------------------------
 ## Look at if was any AE attendance within 24 hours of ambulance time for those not said were conveyed
   
-  all_yas_ae_linked <- merge(epr_single_value_fields_table[, .(epr_ENCRYPTED_HESID, epr_incident_datetime, epr_id)],
-                             ae_data[, .(ae_ENCRYPTED_HESID, ae_ARRIVALTIME, AEKEY)],
+  
+## Merge all epr and all ae records together (includes non-transported to hospital, not ED destination)
+  
+  all_yas_ae_linked <- merge(epr_single_value_fields_table[!is.na(epr_ENCRYPTED_HESID), .(epr_ENCRYPTED_HESID, epr_incident_datetime, epr_id)],
+                             ae_data[, .(ae_ENCRYPTED_HESID, ae_ARRIVALTIME, AEKEY, ae_low_acuity_attendance)],
                              by.x = "epr_ENCRYPTED_HESID",
                              by.y = "ae_ENCRYPTED_HESID",
                              all.x = TRUE) 
   
+
+## Find time between ambulance incident time and AE arrival time, and keep only those within 24 hours
+  
   all_yas_ae_linked[, incident_time_to_arrival_time := as.numeric(difftime(ae_ARRIVALTIME, epr_incident_datetime, unit = "hours"))]
   
   attendance_24_hours <- all_yas_ae_linked[incident_time_to_arrival_time > 0 & incident_time_to_arrival_time <= 24.0]
+  
 
-## eprs that have a AE within 24 hours that are not in linked dataset    
-  setdiff(attendance_24_hours[, epr_id], yas_ae_data[is.na(AEKEY), epr_id])
+## Count number of attendances within 24 hours for each epr
+  
+  attendance_24_hours[, number_ae_24_hour_attendances := sum(.N, na.rm = TRUE), by = epr_id]
+  
+  
+## Where all attendances 'low-acuity'?
+## 'all' works NA how we want; mix/false -> false, mix of true/NA -> NA, all true -> true, 
+  
+  attendance_24_hours[, all_low_acutity_24_hours := all(ae_low_acuity_attendance), by = epr_id]
+  
+  attendance_24_hours <- unique(attendance_24_hours[, .(epr_id, number_ae_24_hour_attendances, all_low_acutity_24_hours)])
+  
+  stopifnot(attendance_24_hours[, .N, by = epr_id][N > 1, .N] == 0)
 
-## AE records that occur within 24 hours of an ambulancec incident that are not in the linked dataset    
-  test <- setdiff(attendance_24_hours[, AEKEY], yas_ae_data[is.na(AEKEY), AEKEY])
-
-  attendance_24_hours[AEKEY %chin% test]
-    
-
+  
+## Merge back into yas_ae table
+  
+  test <- merge(yas_ae_data,
+                attendance_24_hours[, .(epr_id, number_ae_24_hour_attendances, all_low_acutity_24_hours)],
+                by = "epr_id",
+                all.x = TRUE)  
+  
 # Save linked YAS-AE data -------------------------------------------------
 
   save_time <- getDateTimeForFilename()
