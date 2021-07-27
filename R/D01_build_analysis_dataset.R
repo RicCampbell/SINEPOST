98d8998be7f5c655ecf9a7d6b1574f98e7641cb1
Richard Campbell <su_cm1rjcx@sheffield.ac.uk>
2021-07-27 16:05:38 +0100

    Creation of script to bring everything together to create analysis dataset. Includes selection of max NEWS score, and physical obvs so far

---
+++ R/D01_build_analysis_dataset.R
@@ -0,0 +1,194 @@
+## Script for creating analysis dataset
+## Adds fields from other table to 'epr_single..' table
+## Creates and adds derived outcome fields
+
+library(data.table)
+source("R/cleaning_fns_etl.r")
+
+
+## Load in standardised YAS tables, and AE linked YAS data, and ref data
+
+  epr_data_object_names <- load("data/datasets/cohort_processed_epr_table_2021-07-20-170623.rda")
+  yas_ae_data <- readRDS("data/datasets/yas_ae_linked_2021-07-23-134459.rds")
+
+
+# Create deprivation index outcome ----------------------------------------
+
+## ** add in later **
+
+
+
+
+# Create distance to hospital outcome -------------------------------------
+
+## ** add in later **
+
+
+
+
+
+# Add in desired fields from other YAS tables, and add prefix for each table --------
+
+## Take number or records/epr_ids to check merges later
+  
+  epr_id_count <- yas_ae_data[, .N]
+  
+  
+## Find max news scores for each epr_id
+  
+  max_news_score_data <- epr_news_score_table[, .SD[which.max(news_score)], by = epr_id]
+  
+  setnames(max_news_score_data, setdiff(colnames(max_news_score_data), "epr_id"),
+           paste("epr", setdiff(colnames(max_news_score_data), "epr_id"), sep = "_"))
+           
+  stopifnot(uniqueN(epr_news_score_table[, epr_id]) == max_news_score_data[, .N])
+  
+  
+
+# Physical observations ---------------------------------------------------
+
+  ## Remove capital from obvs type so field names are still all lower case later
+  
+  epr_phys_observations_table[, observation_type := tolower(observation_type)]
+  
+  
+## ** Not using MentalCapacity obvs - remember to put in processing doc ** - checked
+  
+## Want to take the Primary observation, and the 'first' Subsequent observation
+
+## Check there is only one Primary set of observations for epr_ids  
+  
+  stopifnot(epr_phys_observations_table[observation_type == "primary", .N, by = epr_id][N > 1, .N] == 0)
+  
+  
+## Make table of just Primary obvs
+  
+  all_primary_obvs <- epr_phys_observations_table[observation_type == "primary"]
+  
+  stopifnot(all_primary_obvs[, .N, by = epr_id][N > 1, .N] == 0)
+
+  
+## Does any epr have more than one record for each Subsequent-datetime pair - YES, max of 3
+  
+  epr_phys_observations_table[, .N, by = .(epr_id, observations_recorded_time)][N > 1, .N, by = N]
+  
+
+## Are these Subsequent-datetime pairs duplicates?
+  ## Get table of the Subsequent-datetime pairs that are the same   
+  
+  yas_duplicated_eprid_obstime <- epr_phys_observations_table[observation_type == "subsequent",
+                                                              N_records := .N, by = .(epr_id, observations_recorded_time)][N_records > 1, epr_id]
+
+  duplicated_fields <- colnames(epr_phys_observations_table)[!(colnames(epr_phys_observations_table) %in% c("epr_id"))]
+  
+  epr_phys_observations_table[epr_id %chin% yas_duplicated_eprid_obstime,
+                              duplicates := duplicated(epr_phys_observations_table[epr_id %chin% yas_duplicated_eprid_obstime], by = duplicated_fields)]
+  
+  epr_phys_observations_table[epr_id %chin% yas_duplicated_eprid_obstime, .(distinct_records = sum(duplicates == FALSE)),
+                                        by = .(epr_id, observations_recorded_time)]
+  
+  ## As [distinct_records != 1, .N] == 0 is NOT TRUE, then they are not duplicates
+  
+  epr_phys_observations_table[, duplicates := NULL]
+
+
+## Build table of Subsequent physical obvs that we want ----------------------------
+
+## Label all subsequent-time pairs in order, first for when there isn't multiple timedate pairs, and then when there is
+    
+    setorder(epr_phys_observations_table, epr_id, observation_type, observations_recorded_time, na.last = TRUE)
+    
+    epr_phys_observations_table[observation_type == "subsequent", subsequent_order := 1:.N, by = .(epr_id)]
+    epr_phys_observations_table[observation_type == "subsequent", subsequent_time_count := .N, by = .(epr_id, observations_recorded_time)]
+    
+  ## Take subsequent phys obvs where first subsequent is unique timedate
+    
+    unique_first_subsequent_obvs <- epr_phys_observations_table[subsequent_order == 1 & subsequent_time_count == 1]
+    
+    stopifnot(unique_first_subsequent_obvs[, .N, by = epr_id][N > 1, .N] == 0)
+    
+  ## Take subsequent phys obvs where first subsequent is NOT unique timedate (== 1 means only have one line per epr_id so have to get all rows for these ids)
+    
+    multiple_first_subsequent_obvs_ids <- epr_phys_observations_table[subsequent_order == 1 & subsequent_time_count > 1, epr_id]
+    
+    multiple_first_subsequent_obvs <- epr_phys_observations_table[(epr_id %chin% multiple_first_subsequent_obvs_ids) & observation_type == "subsequent"]
+  
+    
+  ## Find most non-null set of obvs  
+    
+    multiple_first_subsequent_obvs[, null_fields := rowSums(is.na(multiple_first_subsequent_obvs))]
+    
+    setorder(multiple_first_subsequent_obvs, epr_id, observations_recorded_time, null_fields, na.last = TRUE)
+    
+    multiple_first_subsequent_obvs[, order := 1:.N, by = epr_id]  ##Still need tie breaker but yeah
+    
+    #multiple_first_subsequent_obvs[, .N, by = .(epr_id, observations_recorded_time, null_fields)][N > 1, uniqueN(epr_id)]
+    
+    unique_multiple_first_subsequent_obvs <- multiple_first_subsequent_obvs[order == 1]
+    
+    
+  ## Remove extra cols and bind together
+    
+    unique_first_subsequent_obvs[, c("subsequent_order", "subsequent_time_count", "N_records") := NULL]
+    unique_multiple_first_subsequent_obvs[, c("subsequent_order", "subsequent_time_count", "N_records", "null_fields", "order") := NULL]
+    
+    
+  ## Create id list for subsequent obvs that had multiple records for datetime 
+    
+    unique_multiple_first_subsequent_obvs_ids <- unique_multiple_first_subsequent_obvs[, epr_id]
+    
+    
+  ## Bind all physical obvs that we now how together  
+      
+    all_obvs_want <- rbind(all_primary_obvs, 
+                           rbind(unique_first_subsequent_obvs, unique_multiple_first_subsequent_obvs))
+
+    
+  ## Cast to get wide version
+    
+    phys_obs_cols <- colnames(all_obvs_want)[!(colnames(all_obvs_want) %in% c("epr_id", "observation_type"))]
+    
+    physcial_obvs_wide <- dcast(all_obvs_want, epr_id ~ observation_type, value.var = phys_obs_cols)
+
+    
+  ## Create a field to flag if subsequent obvs had multiple records for datetime
+    
+    physcial_obvs_wide[, multi_timedate_obvs := (epr_id %chin% unique_multiple_first_subsequent_obvs_ids)]
+    
+    
+  ## Create a field to flag if a secondary obvs is prior to primary
+    
+    physcial_obvs_wide[, subsequent_before_primary := (observations_recorded_time_primary > observations_recorded_time_subsequent)]
+    
+  
+
+## Then will want to merge all tables on epr_id
+    
+    epr_id_merge_tables <- list(yas_ae_data,
+                                max_news_score_data,
+                                physcial_obvs_wide)
+
+
+    merged_epr_ids_table <- Reduce(function(x, y) merge(x = x, y = y, by = "epr_id", all.x = TRUE), epr_id_merge_tables)
+    
+  
+## Remove some fields 
+    
+    merged_epr_ids_table[, ("epr_postcode") := NULL]
+    
+    
+## Turn datetime into dates only
+    
+    merged_epr_ids_table[, epr_incident_datetime := as.Date(epr_incident_datetime)]
+    merged_epr_ids_table[, observations_recorded_time_primary := as.Date(observations_recorded_time_primary)]
+    merged_epr_ids_table[, observations_recorded_time_subsequent := as.Date(observations_recorded_time_subsequent)]
+    
+    
+# Save data ---------------------------------------------------------------
+
+  save_time <- getDateTimeForFilename()
+    
+  write.csv(merged_epr_ids_table, file = paste0("data/data_for_analysis/cohort_", save_time, "_yas_ae_phys_obvs.csv"))
+  
+  
+  
